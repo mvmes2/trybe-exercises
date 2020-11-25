23ffae31da144cdb9ec2f5400444f89a4b810784
mvmes2 <mvmes23@gmail.com>
2020-11-25 18:24:54 -0300

    exercício 4 ok

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio4.js
@@ -0,0 +1,10 @@
+/*Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+let valor = 0;
+
+if(valor > 0){
+    console.log('positive');
+}else if(valor < 0){
+    console.log('negative');
+}else if(valor == 0){
+    console.log('zero');
+}
