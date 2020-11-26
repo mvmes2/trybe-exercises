f0e43fea4966fc3aeab529c5328af2602f28b18c
mvmes2 <mvmes23@gmail.com>
2020-11-26 14:40:42 -0300

    finalizado exercício 9

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio9.js
@@ -0,0 +1,11 @@
+/* Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+let a = 8;
+let b = 4;
+let c = 6;
+isOdd = false;
+
+if(a%2!=0 | b%2!=0 | c%2!=0){
+    isOdd = true;    
+}
+console.log(isOdd);
