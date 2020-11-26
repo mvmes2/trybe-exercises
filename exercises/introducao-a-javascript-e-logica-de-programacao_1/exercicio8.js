11a27e2511200b7a68e2312213bb1064dbbe4419
mvmes2 <mvmes23@gmail.com>
2020-11-26 14:32:53 -0300

    finalizado exercício 8

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio8.js
@@ -0,0 +1,12 @@
+/* Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+
+let a = 3;
+let b = 4;
+let c = 6;
+
+
+if(a%2==0 & b%2==0 | c%2==0){
+    console.log('true');
+}else
+ console.log('false');
