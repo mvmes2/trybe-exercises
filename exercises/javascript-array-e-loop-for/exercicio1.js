17024602166a721be12f8b7526a226d0a9bd9bbd
mvmes2 <mvmes23@gmail.com>
2020-11-28 07:41:18 -0300

    finalizado exercicio 1

---
+++ exercises/javascript-array-e-loop-for/exercicio1.js
@@ -0,0 +1,9 @@
+/*Leia atentamente os enunciados e faça o que se pede! Você irá utilizar esse array para realizar os exercícios do 1 ao 7: 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let index;
+for(index = 0 ; index <numbers.length; index +=1){
+    console.log(numbers[index]);    
+}
