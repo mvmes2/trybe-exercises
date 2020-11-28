a88f8edaf98991113bc6d9e5d56bf31a87bd7011
mvmes2 <mvmes23@gmail.com>
2020-11-28 08:05:09 -0300

    finalizado exercicio 2

---
+++ exercises/javascript-array-e-loop-for/exercicio2.js
@@ -0,0 +1,11 @@
+/*Leia atentamente os enunciados e faça o que se pede! Você irá utilizar esse array para realizar os exercícios do 1 ao 7: 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+2 - Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let index;
+let value = null;
+for(index = 0; index < numbers.length; index +=1){
+   value = value +(numbers[index]);   
+}
+console.log(value);
