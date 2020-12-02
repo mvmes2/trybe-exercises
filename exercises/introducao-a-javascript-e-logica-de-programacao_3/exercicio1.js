f8a91a5b4df2f10ee9440701e965b249c5d3613b
mvmes2 <mvmes23@gmail.com>
2020-12-02 12:18:51 -0300

    exercicio 2 finalizado, melhorias no exercicio 1

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -2,16 +2,17 @@
 let n = 4;
 let simbol = '';
 
-// for(let linha = 0; linha <=n; linha+=1)
-// {
-//     simbol += '*';
-//     console.log(simbol);
+
+// for(let linha = 0; linha <=n*n; linha+=1){    
+//     if(linha%n === 0){
+//         console.log(simbol);
+//         simbol= '';
+//     }  
+//     simbol+= '*';  
 // }
 
-for(let linha = 0; linha <=n*n; linha+=1){    
-    if(linha%n === 0){
-        console.log(simbol);
-        simbol= '';
-    }  
-    simbol+= '*';  
+
+
+for(let linha =0; linha <n; linha+=1){     // dica da turma... são uns Gênios..eu  nao conhecia o repeat.
+     console.log('*'.repeat(n));
 }
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio2.js
@@ -0,0 +1,7 @@
+/*2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo: */
+let n = 5;
+let symbol = '';
+for(linha = 0; linha <=n;linha+=1){
+    symbol+='*'
+    console.log(symbol);
+}
