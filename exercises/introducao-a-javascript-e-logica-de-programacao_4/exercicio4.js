88ff2ae5dd95445d26f631e5bb4b3ab564a8666d
mvmes2 <mvmes23@gmail.com>
2020-12-08 15:21:02 -0300

    finalizado exercicio 4

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio4.js
@@ -0,0 +1,22 @@
+/*
+Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+*/
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+  info['recorrente'] = 'sim';
+  for(let keys in info){
+      console.log(info[keys]);
+  }
