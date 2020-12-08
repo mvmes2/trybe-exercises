0f52303fc2409dcba707770957bb5cfb5db2ff9f
mvmes2 <mvmes23@gmail.com>
2020-12-08 15:38:13 -0300

    finalizado exercicio 5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio5.js
@@ -0,0 +1,28 @@
+/*
+Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+*/
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",    
+  };
+
+  info['recorrente'] = 'sim';
+  info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: 'sim'
+     }
+  
+      console.log(info,info2);
+  
